7ab9c09f3a838975d23dde4816170feb690fba08
Angel75Git <aduboncenteno@usf.edu>
2025-02-08 13:26:57 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -5,3 +5,12 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){
 //Test Data to ensure function is correct
 console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`)
 console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`)
+
+//Task 2
+function calculateSalesTax(amount, taxRate) {
+    return (amount * taxRate).toFixed(2)
+}//Included toFixed so there wouldn't be any lingering zeros
+//Logging tax for varying amounts
+console.log(`Sales Tax: $${calculateSalesTax(100, .07)}`)
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)
+
